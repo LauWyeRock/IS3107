f125775a3b8654d30f259380c6898c3b5fa58bd6
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2023-04-21 03:24:56 +0800

    Removed old files

--- airflow/dags/glenmond/params.py
+++
@@ -1,8 +0,0 @@
-google_cloud_path = '/home/airflow/is3107/src/test-344015-02e727f034f5.json'
-project_id = 'test-344015'
-staging_dataset = 'TEST_DWH_STAGING'
-dwh_dataset = 'TEST_DWH'
-gs_bucket = 'is3107_bucket_test'
-model_path = '/home/airflow/dags/sentiment_model/model.pickle'
-vectorizer_path = '/home/airflow/dags/sentiment_model/vectorizer.pickle'
-alert_email_receipient = ''
